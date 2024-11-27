e42e2f1edfcd18ef86e8565f0bb0d0fc3e7bd50d
leoraisilva <lraimundo409@gmail.com>
2024-11-27 01:29:38 -0300

    comunicando com api cliente

--- kraken-front/src/App.jsx
+++ kraken-front/src/App.jsx
@@ -3,17 +3,24 @@ import './App.css'
 import Cadastro from './pages/home/Cadastro'
 import Home from './pages/home/Home'
 import Category from './components/category/Category'
+import AuthGuard from './components/Login/AuthGuard' 
+import Login from './components/Login/Login'
 
 const page = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
+    path: '/kraken/',
+    element: (
+      <AuthGuard requireAuth={true}>
+        <Home />
+      </AuthGuard>
+    ),
     children: [
-      { path: 'categoria', element: <Category />},
-      { path: 'cadastro', element: <Cadastro /> }
+      { path: 'categoria', element: <Category />}
     ]
   },
+  { path: '/', element: <Login /> },
+  { path: '/cadastro', element: <Cadastro /> },
   { path: "*", element: <div>404 - Página não encontrada</div> } 
 ])
 
-export { page } 
+export { page }
--- kraken-front/src/components/setup/Cadastrar.jsx
+++ kraken-front/src/components/setup/Cadastrar.jsx
@@ -1,59 +1,114 @@
+import { useState } from 'react';
 import './cadastrar.css'
 
 function Cadastrar () {
+
+    const [usuario, setUsuario] = useState('');
+    const [nome, setNome] = useState('');
+    const [senha, setSenha] = useState('');
+    const [email, setEmail] = useState('');
+    const [tell, setTell] = useState('');
+    const [cep, setCep] = useState('');
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const dataHora = new Date();
+    
+        const cliente = {
+            usuario: usuario,
+            nome: nome,
+            senha: senha,
+            email: email,
+            tell: tell,
+            cep: cep,
+            dataCadastro: dataHora.toISOString()
+        }
+
+        try {
+            const response = await fetch('http://localhost:8080/cliente',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(cliente)
+            })
+
+            if (!response.ok) { 
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+
+            const result = await response.json();
+            
+            setUsuario('');
+            setNome('');
+            setSenha('');
+            setEmail('');
+            setTell('');
+            setCep('');
+
+        }catch (error) {
+            setError('Error 403: Problema ao cadastrar. Tente novamente mais tarde.')
+        }
+    }
+
+
     return (
         <>
             <div className='setting-container'>
                 <h1>Setting:</h1>
                 <div className='setup-container'>
-                    <div className='setup-content'>
-                        <h2>Usuario:</h2>
-                        <input type='text'  />
-                        <h2>Nome:</h2>
-                        <input type='text' />
-                        <h2>Senha:</h2>
-                        <input type='text' />
-                        
-                        <div className='address-setup'>
-                            <h3>Endereço:</h3>
-                            <div className='address-components'>
-                                <h2>Cep:</h2>
-                                <input type='text'  />
+                    <form onSubmit={handleSubmit} >
+                        <div className='setup-content'> 
+                            <h2>Usuario:</h2>
+                            <input type='text' id='usuario' value={usuario} onChange={(e) => setUsuario(e.target.value)} />
+                            <h2>Nome:</h2>
+                            <input type='text' id='nome' value={nome} onChange={(e) => setNome(e.target.value)} />
+                            <h2>Senha:</h2>
+                            <input type='password' id='senha' value={senha} onChange={(e) => setSenha(e.target.value)} />
+                            
+                            <div className='address-setup'>
+                                <h3>Endereço:</h3>
+                                <div className='address-components'>
+                                    <h2>Cep:</h2>
+                                    <input type='text' id='cep' value={cep} onChange={(e) => setCep(e.target.value)} />
+                                </div>
+                                <div className='address-components'>
+                                    <h2>Pais:</h2>
+                                    <input type='text' />
+                                </div>
+                                <div className='address-components'>
+                                    <h2>Cidade:</h2>
+                                    <input type='text'  />
+                                </div>
+                                <div className='address-components'>
+                                    <h2>Estado:</h2>
+                                    <input type='text' />
+                                    <h2>UF:</h2>
+                                    <input type='text'  />
+                                </div>
+                                <div className='address-components'>
+                                    <h2>Rua:</h2>
+                                    <input type='text' />
+                                </div>
+                                <div className='address-components'>
+                                    <h2>Bairro:</h2>
+                                    <input type='text'/>
+                                </div>
+                                <div className='address-components'>
+                                    <h2>Número:</h2>
+                                    <input type='text'/>
+                                </div>
                             </div>
-                            <div className='address-components'>
-                                <h2>Pais:</h2>
-                                <input type='text' />
-                            </div>
-                            <div className='address-components'>
-                                <h2>Cidade:</h2>
-                                <input type='text'  />
-                            </div>
-                            <div className='address-components'>
-                                <h2>Estado:</h2>
-                                <input type='text' />
-                                <h2>UF:</h2>
-                                <input type='text'  />
-                            </div>
-                            <div className='address-components'>
-                                <h2>Rua:</h2>
-                                <input type='text' />
-                            </div>
-                            <div className='address-components'>
-                                <h2>Bairro:</h2>
-                                <input type='text'/>
-                            </div>
-                            <div className='address-components'>
-                                <h2>Número:</h2>
-                                <input type='text'/>
-                            </div>
-                        </div>
 
-                        <h2>E-mail:</h2>
-                        <input type='text'/>
-                        <h2>Tell:</h2>
-                        <input type='text' />
-                        <button className='setting-btn'>Confirmar</button>
-                    </div>
+                            <h2>E-mail:</h2>
+                            <input type='text' id='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                            <h2>Tell:</h2>
+                            <input type='text' id='tell' value={tell} onChange={(e) => setTell(e.target.value)} />
+                            <button type='submit' className='setting-btn' >Confirmar</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
