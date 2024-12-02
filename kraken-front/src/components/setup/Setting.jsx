fbd3d60c82ba51ad1a556aa9dfe1f73c18ebb76e
leoraisilva <lraimundo409@gmail.com>
2024-12-01 22:54:53 -0300

    alteração no client

--- kraken-front/src/components/setup/Setting.jsx
+++ kraken-front/src/components/setup/Setting.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate  } from 'react-router-dom';
 import './cadastrar.css'
-import Delete from './Delete';
 
 function Setting () {
 
