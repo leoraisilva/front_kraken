27e6f4710f80104c065b0f6357edbfbee76fd98b
vpm001plx <E961085@iberdrola.com>
2025-02-06 17:51:34 -0300

    atualizando a api produto

--- kraken-front/src/components/body/register/Register.jsx
+++ kraken-front/src/components/body/register/Register.jsx
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
+import { useNavigate, Link } from 'react-router-dom';
 import "../category.css";
-import { data, Link } from 'react-router-dom';
 
 function Register () {
-    const [categ, setCateg] = useState([])
-    const [error, setError] = useState('')
+    const [categ, setCateg] = useState([]);
+    const [error, setError] = useState('');
+    const [store, setStore] = useState([]);
+    const navigate = useNavigate();
+    const [nomeProduto, setNomeProduto] = useState('');
+    const [descricao, setDescricao] = useState('');
+    const [categoriaId, setCategoriaId] = useState('');
+    const [valorUnitario, setValorUnitario] = useState('');
+    const [estoqueid, setEstoqueid] = useState('');
+    const [quantidadeProduto, setQuantidadeProduto] = useState('');
+    const [image, setImage] = useState(null);
     
     useEffect(()=> {
         fetch(`http://localhost:8081/categoria`, {
@@ -23,65 +32,108 @@ function Register () {
             console.error("erro ao carregar page", error);
             setError("Erro na leitura dos dados");
         })
-    })
+    }, []);
 
+    useEffect(() => {
+        fetch(`http://localhost:8082/estoque`, {
+            method: 'GET',
+            mode: 'cors'
+        })
+        .then(res => {
+            if(!res.ok)
+                throw new Error("Erro ao inicializar")
+            return res.json()
+        })
+        .then(data=> {
+            setStore(data);
+        })
+        .catch(error => {
+            console.error("Erro ao iniciar o dados", error);
+            setError("Erro na leitura dos dados");
+        })
+    }, []);
+
+    const handleProductRegister = async (k) => {
+        k.preventDefault();
+
+        const formData = new FormData();
+        formData.append('nomeProduto', nomeProduto);
+        formData.append('descricao', descricao);
+        formData.append('valorUnitario', valorUnitario);
+        formData.append('categoriaId', categoriaId);
+        formData.append('estoque_id', estoqueid);
+        formData.append('quantidadeProduto', quantidadeProduto);
+        formData.append('image', image);
+
+        console.log(formData);
+        try{
+            const response = await fetch(`http://localhost:8083/produto`, {
+                method: 'POST',
+                body: formData
+            })
+            if (!response.ok) 
+                throw new Error(`Erro na requisição ${response.status}`);
+            const result = await response.json();
+            
+            navigate('/kraken/'); 
+        } catch (Error) {
+            setError('Erro no cadastro', error);
+            console.error(error);
+        }
+    }
 
     return (
         <>
         <div className="container-setting">
             <h2>Cadastrar</h2>
-            <form class="row g-3">
+            <form className="row g-3" onSubmit={handleProductRegister}>
                 
-                <div class="col-12">
-                    <label for="inputAddress" class="form-label">Nome do Produto</label>
-                    <input type="text" class="form-control" id="inputAddress" />
+                <div className="col-12">
+                    <label className="form-label">Nome do Produto</label>
+                    <input type="text" className="form-control" onChange={(e) => setNomeProduto(e.target.value)} required />
                 </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label">Descrição do Produto</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <div className="mb-3">
+                    <label className="form-label">Descrição do Produto</label>
+                    <textarea className="form-control" onChange={(e) => setDescricao(e.target.value)} rows="3" required></textarea>
                 </div>
-                <div class="mb-3">
-                    <label for="formFileMultiple" class="form-label">Imagem</label>
-                    <input class="form-control" type="file" id="formFileMultiple" multiple />
+                <div className="mb-3">
+                    <label className="form-label">Imagem</label>
+                    <input className="form-control" type="file" onChange={(e) => setImage(e.target.files[0])} required />
                 </div>
-                <div class="col-md-6">
-                    <label for="inputCity" class="form-label">Valor Unitário</label>
-                    <input type="text" class="form-control" id="inputCity" />
+                <div className="col-md-12">
+                    <label className="form-label">Valor Unitário</label>
+                    <input type="text" className="form-control" onChange={(e) => setValorUnitario(e.target.value)} required />
                 </div>
-                <div class="col-md-6">
-                    <label for="inputZip" class="form-label">Unidade Utilizada</label>
-                    <input type="text" class="form-control" id="inputZip"/>
-                </div>
-                <div class="col-md-12">
-                    <label for="inputCity" class="form-label">Categoria</label>
-                        <select class="form-select" aria-label="Default select example">
+                <div className="col-md-12">
+                    <label className="form-label">Categoria</label>
+                        <select className="form-select" aria-label="Default select example" value={categoriaId} onChange={(e) => setCategoriaId(e.target.value)} required >
                             <option selected>Seleciona a Categoria</option>
                             {categ.map((item, index)=>(
-                                <option key={index}>{item.titulo}</option>
+                                <option value={item.categoriaId} key={index}>{item.titulo}</option>
                             ))}
                         </select>
                 </div>
-                <div class="col-md-12">
-                    <p><Link to="/kraken/registro-categoria" class="link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Registrar a Categoria</Link></p>
+                <div className="col-md-12">
+                    <p><Link to="/kraken/registro-categoria" className="link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Registrar a Categoria</Link></p>
                 </div>
-                <div class="col-md-12">
-                    <label for="inputCity" class="form-label">Estoque</label>
-                    <select class="form-select" aria-label="Default select example">
+                <div className="col-md-12">
+                    <label className="form-label">Estoque</label>
+                    <select className="form-select" aria-label="Default select example" value={estoqueid} onChange={(e) => setEstoqueid(e.target.value)} required >
                         <option selected>Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        {store.map((produto, index) => (
+                            <option value={produto.estoqueid} key={index}>{produto.nomeEstoque}</option>
+                        ))}
                     </select>
                 </div>
-                <div class="col-md-12">
-                    <label for="inputMax" class="form-label">Quantidade Registrado</label>
-                    <input type="number" class="form-control" id="inputMax" />
+                <div className="col-md-12">
+                    <label className="form-label">Quantidade Registrado</label>
+                    <input type="number" className="form-control" onChange={(e) => setQuantidadeProduto(e.target.value)} required />
                 </div>
-                <div class="col-md-12">
-                    <p><Link to="/kraken/store" class="link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Registrar Estoque</Link></p>
+                <div className="col-md-12">
+                    <p><Link to="/kraken/store" className="link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Registrar Estoque</Link></p>
                 </div>
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Sign in</button>
+                <div className="col-12">
+                    <button type="submit" className="btn btn-primary">Sign in</button>
                 </div>
             </form>
         </div>
--- kraken-front/src/components/body/register/Store.jsx
+++ kraken-front/src/components/body/register/Store.jsx
@@ -1,33 +1,74 @@
+import { useState } from 'react';
 import '../category.css';
+import { useNavigate } from 'react-router-dom';
 
 function Store (){
+    const [nome, setNome] = useState('')
+    const [minimo, setMinimo] = useState(0)
+    const [maximo, setMaximo] = useState(0)
+    const [quantidade, setQuantidade] = useState(0)
+    const [descricao, setDescricao] = useState('')
+    const [error, setError] = useState('')
+    const navigate = useNavigate();
+
+    const handleStore = async (h) => {
+        h.preventDefault()
+
+        const store = {
+            nomeEstoque: nome,
+            valorMinimo: minimo,
+            valorMaximo: maximo,
+            quantidadeProduto: quantidade,
+            descricao: descricao
+        }
+
+        try{
+            const response = await fetch(`http://localhost:8082/estoque`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(store)
+            })
+            if(!response.ok)
+                throw new Error("Erro ao tentar cadastrar")
+            const result = await response.json()
+            
+        }catch (error) {
+            setError("Erro ao cadastrar");
+            console.error(error)
+        }
+        navigate('/kraken/'); 
+    }
+
+
     return (
         <>
         <div className="container-setting">
             <h2>Cadastro Estoque</h2>
-            <form class="row g-3">
-                <div class="col-12">
-                    <label for="inputAddress" class="form-label">Nome do Estoque</label>
-                    <input type="text" class="form-control" id="inputAddress" />
+            <form className="row g-3" onSubmit={handleStore}>
+                <div className="col-12">
+                    <label className="form-label">Nome do Estoque</label>
+                    <input type="text" className="form-control" onChange={(e) => setNome(e.target.value)} />
                 </div>
-                <div class="col-md-6">
-                    <label for="inputMax" class="form-label">Valor mínimo</label>
-                    <input type="number" class="form-control" id="inputMax" />
+                <div className="col-md-6">
+                    <label className="form-label">Valor mínimo</label>
+                    <input type="number" className="form-control" onChange={(e) => setMinimo(e.target.value)} />
                 </div>
-                <div class="col-md-6">
-                    <label for="inputMin" class="form-label">Valor máximo</label>
-                    <input type="number" class="form-control" id="inputMin"/>
+                <div className="col-md-6">
+                    <label className="form-label">Valor máximo</label>
+                    <input type="number" className="form-control" onChange={(e) => setMaximo(e.target.value)}/>
                 </div>
-                <div class="col-12">
-                    <label for="qntItem" class="form-label">Quantidade Maxima de item</label>
-                    <input type="number" class="form-control" id="qntItem" />
+                <div className="col-12">
+                    <label className="form-label">Quantidade Maxima de item</label>
+                    <input type="number" className="form-control" onChange={(e) => setQuantidade(e.target.value)} />
                 </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label">Descrição do Estoque</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <div className="mb-3">
+                    <label className="form-label">Descrição do Estoque</label>
+                    <textarea className="form-control" onChange={(e) => setDescricao(e.target.value)} rows="3"></textarea>
                 </div>
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Sign in</button>
+                <div className="col-12">
+                    <button type="submit" className="btn btn-primary">Cadastrar</button>
                 </div>
             </form>
         </div>
